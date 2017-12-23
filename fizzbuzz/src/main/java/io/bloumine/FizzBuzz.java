b6842efeaa892f6cd703670f043fcf747c91b1a4
bloumix <xavier.bloumine@gmail.com>
2017-12-23 22:28:18 +0100

    FizzBuzz refactor

--- fizzbuzz/src/main/java/io/bloumine/FizzBuzz.java
+++ fizzbuzz/src/main/java/io/bloumine/FizzBuzz.java
@@ -2,16 +2,29 @@ package io.bloumine;
 
 public class FizzBuzz {
     public String generateString(int number) {
-
-        if (number % 3 == 0 && number % 5 == 0)
-            return "fizzbuzz";
-
-        if (number % 3 == 0)
-            return "fizz";
-
-         if (number % 5 == 0)
-             return "buzz";
+        if (isMultipleOf3Or5(number))
+            return fizzIfMultipleOf3(number) + buzzIfMultipleOf5(number);
 
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf3Or5(int number) {
+        return isMultipleOf3(number) || isMultipleOf5(number);
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
+    }
+
+    public String buzzIfMultipleOf5(int number) {
+        return isMultipleOf5(number) ? "buzz" : "";
+    }
+
+    public String fizzIfMultipleOf3(int number) {
+        return isMultipleOf3(number) ? "fizz" : "";
     }
 }
