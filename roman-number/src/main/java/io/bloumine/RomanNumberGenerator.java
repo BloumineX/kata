872a1278212d9286ca4888cca060862aa3395376
bloumix <xavier.bloumine@gmail.com>
2017-12-25 10:10:25 +0100

    roman-numer should_return_up_to_3_I_when_number_is_up_to_3 refactor

--- roman-number/src/main/java/io/bloumine/RomanNumberGenerator.java
+++ roman-number/src/main/java/io/bloumine/RomanNumberGenerator.java
@@ -1,13 +1,27 @@
 package io.bloumine;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class RomanNumberGenerator {
     public String generateToRomanNumber(int arabicNumber) {
+        StringBuilder romanNumber = new StringBuilder();
+
         if (arabicNumber == 5)
-            return "V";
-        if (arabicNumber == 3)
-            return "III";
-        if (arabicNumber == 2)
-            return "II";
-        return "I";
+            romanNumber.append("V");
+
+        if (arabicNumber <= 3)
+            romanNumber.append(getIWhenNumberInfTo3(arabicNumber));
+
+        return romanNumber.toString();
+    }
+
+    private String getIWhenNumberInfTo3(int arabicNumber) {
+        return  IntStream.iterate(1, number -> number + 1)
+                .limit(arabicNumber)
+                .mapToObj(arabic -> "I")
+                .collect(Collectors.joining(""));
+
+
     }
 }
