2ca8ad16974cadcaf0e1f49bca43800c20e1e149
bloumix <xavier.bloumine@gmail.com>
2017-12-25 19:50:02 +0100

    roman-numer refactor
    independant to ordinal Enum

--- roman-number/src/main/java/io/bloumine/RomanNumberGenerator.java
+++ roman-number/src/main/java/io/bloumine/RomanNumberGenerator.java
@@ -1,15 +1,15 @@
 package io.bloumine;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RomanNumberGenerator {
 
     public String generateToRomanNumber(int arabicNumber) {
         StringBuilder romanNumber = new StringBuilder();
 
-        for (RomanNumeral roman : gerReverseRomanNumeralList()) {
+        for (RomanNumeral roman : getRomanLetterOrderedDescFromValue()) {
             romanNumber.append(roman.numeralRomanFromNumberIfExist(arabicNumber));
             arabicNumber = roman.returnArabicValueMinusRomanIfPossible(arabicNumber);
         }
@@ -17,9 +17,13 @@ public class RomanNumberGenerator {
         return romanNumber.toString();
     }
 
-    private List<RomanNumeral> gerReverseRomanNumeralList() {
-        List<RomanNumeral> romanNumerals = Arrays.asList(RomanNumeral.values());
-        Collections.reverse(romanNumerals);
-        return romanNumerals;
+    private List<RomanNumeral> getRomanLetterOrderedDescFromValue() {
+        return Stream.of(RomanNumeral.values())
+                .sorted(this::getHighestArabicValueFrom)
+                .collect(Collectors.toList());
+    }
+
+    private int getHighestArabicValueFrom(RomanNumeral numeralLetter1, RomanNumeral numeralLetter2) {
+        return numeralLetter2.getArabicValue() - numeralLetter1.getArabicValue();
     }
 }
